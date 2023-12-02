20c1eb805111a99f0446c557a3060fb4cbf6b52d
polemon <polemon@gmail.com>
2023-12-03 00:25:45 +0100

    finished day 2

--- day02.py
+++ day02.py
@@ -4,14 +4,31 @@ import sys
 import re
 
 def get_num(line):
-    print(line.strip().split(": ")[1].split('; '))
-    return 0
+    r_max = 0
+    g_max = 0
+    b_max = 0
+    
+    for grab in line.strip().split(": ")[1].split('; '):
+        try:
+            r_max = max(r_max, int(re.search(r"(\d+) red", grab).group(1)))
+        except:
+            pass
+
+        try:
+            g_max = max(g_max, int(re.search(r"(\d+) green", grab).group(1)))
+        except:
+            pass
+        
+        try:
+            b_max = max(b_max, int(re.search(r"(\d+) blue", grab).group(1)))
+        except:
+            pass
+
+    return (r_max * g_max * b_max)
 
 def main():
     with open("day02.in.utf8", 'r') as FILE:
-        with open("day02.out.utf8", 'w') as O_FILE:
-            O_FILE.write(f"{sum(map(get_num, FILE.readlines()))}\n")
-            O_FILE.close()
+        print(f"{sum(map(get_num, FILE.readlines()))}")
         FILE.close()
 
 if __name__ == "__main__":

c6634f78d2bc28676e139b6766fc44a31bfa3756
polemon <polemon@users.noreply.github.com>
2023-12-03 13:37:38 +0100

    beginning day 3

---
+++ day03.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import sys
+import re
+import numpy as np
+
+def pad_dots(vec, pad_width, iaxis, kwargs):
+    vec[:1] = '.'
+    vec[-1:] = '.'
+
+
+
+def main()
+    schematic = np.loadtxt("./day03.in.utf8", dtype=bytes, comments=None)
+
+    
+
+
+if __name__ == "__main__":
+    main()

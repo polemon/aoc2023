f438e3072b4c28d39d861e592e0b633c89da9966
polemon <polemon@users.noreply.github.com>
2023-12-03 21:12:46 +0100

    added day 3 part 1

--- day03.py
+++ day03.py
@@ -4,17 +4,30 @@ import sys
 import re
 import numpy as np
 
-def pad_dots(vec, pad_width, iaxis, kwargs):
-    vec[:1] = '.'
-    vec[-1:] = '.'
+def pad_dots(schematic):
+    padded_tb = np.concatenate([ np.array([ b'.' * len(schematic[0]) ]), schematic, np.array([ b'.' * len(schematic[0]) ]) ])
+    padlr = np.vectorize(lambda x: b'.' + x + b'.')
+    padded = padlr(padded_tb)
+    return padded
 
+def main():
+    p_schem = pad_dots( np.loadtxt("./day03.in.utf8", dtype=bytes, comments=None) )
+    pat_num = re.compile(r"\d+")
+    pat_dots = re.compile(r"^\.+$")
 
+    s=0
 
-def main()
-    schematic = np.loadtxt("./day03.in.utf8", dtype=bytes, comments=None)
+    for (k, v) in enumerate(p_schem):
+        for m in pat_num.finditer(v.decode()):
+            top = p_schem[k-1][m.start()-1:m.start() + len(m.group()) + 1]
+            mid = p_schem[ k ][m.start()-1:m.start() + len(m.group()) + 1]
+            bot = p_schem[k+1][m.start()-1:m.start() + len(m.group()) + 1]
+            checkstr = top + mid[:1] + mid[-1:] + bot
+            
+            if not pat_dots.search(checkstr.decode()):
+                s = s + int(m.group())
 
-    
-
+    print(f"{s}")
 
 if __name__ == "__main__":
     main()
